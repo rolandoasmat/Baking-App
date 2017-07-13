e50f19fe214ce5d00c872238a3b1aed45920b97f
Rolando Asmat <rolandoasmat@gmail.com>
2017-07-12 22:52:22 -0500

    extremely basic view pager implemented

---
+++ app/src/main/java/com/asmat/rolando/bakingapp/adapters/RecipeDetailsPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.asmat.rolando.bakingapp.adapters
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.asmat.rolando.bakingapp.fragments.IngredientsFragment
+import com.asmat.rolando.bakingapp.models.Recipe
+
+
+/**
+ * Created by rolandoasmat on 7/12/17.
+ */
+
+
+class RecipeDetailsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    var recipe: Recipe? = null
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return IngredientsFragment.newInstance(recipe!!.ingredients)
+    }
+}
