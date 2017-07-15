9a6f2dbd360897d8a543f7f41654a8c48d85c4e7
Rolando Asmat <rolandoasmat@gmail.com>
2017-07-15 14:28:25 -0500

    MediaSession implemented

--- app/src/main/java/com/asmat/rolando/bakingapp/fragments/CompleteStepFragment.kt
+++ app/src/main/java/com/asmat/rolando/bakingapp/fragments/CompleteStepFragment.kt
@@ -3,6 +3,8 @@ package com.asmat.rolando.bakingapp.fragments
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +21,7 @@ import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.source.TrackGroupArray
@@ -26,8 +29,10 @@ import com.google.android.exoplayer2.source.TrackGroupArray
 class CompleteStepFragment : Fragment(), ExoPlayer.EventListener {
     private var mRecipe: Recipe? = null
     private var mPosition: Int? = null
-    var simpleExoPlayerView: SimpleExoPlayerView? = null
-    var player: SimpleExoPlayer? = null
+    private var simpleExoPlayerView: SimpleExoPlayerView? = null
+    private var mPlayer: SimpleExoPlayer? = null
+    private var mMediaSession: MediaSessionCompat? = null
+    private val TAG = "CompleteStepFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,72 +60,123 @@ class CompleteStepFragment : Fragment(), ExoPlayer.EventListener {
             val bandwidthMeter = DefaultBandwidthMeter()
             val videoTrackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
             val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
-            player = ExoPlayerFactory.newSimpleInstance(context, trackSelector)
-            simpleExoPlayerView?.player = player!!
+            mPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector)
+            simpleExoPlayerView?.player = mPlayer!!
             val dataSourceFactory = DefaultDataSourceFactory(context, Util.getUserAgent(context, "BakingApp"), bandwidthMeter)
             val extractorsFactory = DefaultExtractorsFactory()
             val uri = Uri.parse(uriString)
             val videoSource = ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null)
-            player?.addListener(this)
-            player?.prepare(videoSource)
+            mPlayer?.addListener(this)
+            mPlayer?.prepare(videoSource)
             simpleExoPlayerView?.requestFocus()
+            setupMediaSession()
         }
         return view
     }
 
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (isVisibleToUser) {
+            mMediaSession?.isActive = true
+        } else {
+            mPlayer?.playWhenReady = false
+            mMediaSession?.isActive = false
+        }
+    }
+
     override fun onPause() {
         super.onPause()
-        if (player != null) {
-            player?.playWhenReady = false //to pause a video because now our video player is not in focus
-        }
+        mPlayer?.playWhenReady = false
+        mMediaSession?.isActive = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMediaSession?.isActive = true
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        player?.release()   //it is important to release a player
+        mPlayer?.release()
+        mMediaSession?.release()
     }
+
+    fun setupMediaSession() {
+        mMediaSession = MediaSessionCompat(context, "CompleteStepFragment")
+
+        val flags = MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
+        mMediaSession?.setFlags(flags)
+        mMediaSession?.setMediaButtonReceiver(null)
+
+        val actions = PlaybackStateCompat.ACTION_PLAY or PlaybackStateCompat.ACTION_PAUSE or
+                PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+        val initialStateBuilder = PlaybackStateCompat.Builder().setActions(actions)
+
+        mMediaSession?.setPlaybackState(initialStateBuilder.build())
+        mMediaSession?.setCallback(MySessionCallback())
+        mMediaSession?.isActive = true
+    }
+
+    // ExoPlayer.EventListener overrides
 
     override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
         when (playbackState) {
             ExoPlayer.STATE_BUFFERING ->
-                print("STATE_BUFFERING")
+                Log.v(TAG, "STATE_BUFFERING")
             ExoPlayer.STATE_IDLE ->
-                print("STATE_IDLE")
+                Log.v(TAG, "STATE_IDLE")
             ExoPlayer.STATE_READY -> {
+                Log.v(TAG, "STATE_READY")
+                var state: Int
                 if(playWhenReady) {
-                    // We are playing
+                    Log.v(TAG, "We are playing")
+                    state = PlaybackStateCompat.STATE_PLAYING
                 } else {
-                    // We are paused
-
+                    Log.v(TAG, "We are paused")
+                    state = PlaybackStateCompat.STATE_PAUSED
+                }
+                // Update session state
+                val player = mPlayer
+                if(player != null) {
+                    val stateBuilder = PlaybackStateCompat.Builder().setState(state, player.currentPosition, 1f)
+                    mMediaSession?.setPlaybackState(stateBuilder.build())
                 }
             }
             ExoPlayer.STATE_ENDED ->
-                print("STATE_ENDED")
+                Log.v(TAG, "STATE_ENDED")
         }
     }
 
-    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
-        //
-    }
+    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) { }
 
-    override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) { }
 
-    override fun onPlayerError(error: ExoPlaybackException?) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onPlayerError(error: ExoPlaybackException?) { }
 
-    override fun onLoadingChanged(isLoading: Boolean) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onLoadingChanged(isLoading: Boolean) { }
 
-    override fun onPositionDiscontinuity() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onPositionDiscontinuity() { }
 
-    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) { }
+
+    // MediaSession Callbacks. This is how external clients can interact with the player.
+
+    inner class MySessionCallback: MediaSessionCompat.Callback() {
+
+        override fun onPlay() {
+            super.onPlay()
+            mPlayer?.playWhenReady = true
+        }
+
+        override fun onPause() {
+            super.onPause()
+            mPlayer?.playWhenReady = false
+        }
+
+        override fun onSkipToPrevious() {
+            super.onSkipToPrevious()
+            mPlayer?.seekTo(0)
+        }
     }
 
     companion object {
